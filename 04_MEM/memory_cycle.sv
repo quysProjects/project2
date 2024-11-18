cee80c1bb2d6b9e4b2372b0ca0f0ab56f171797c
RishSky <140881264+RishSky@users.noreply.github.com>
2024-11-18 20:25:14 +0700

    new memory_cycle

--- 04_MEM/memory_cycle.sv
+++ 04_MEM/memory_cycle.sv
@@ -1,5 +1,8 @@
 import riscv_types::*;
-module memory_cycle(
+module memory_cycle#(
+    parameter ADDR_WIDTH = 32,
+    parameter DATA_WIDTH = 32
+)(
 	input  logic 		clk_i				,
 	input  logic 		rst_ni				,
 	input  memory_info	memory_signals		,
@@ -23,6 +26,27 @@ module memory_cycle(
 	output logic [31:0] io_ledg_o			,
 	output logic [31:0] io_lcd_o
 );
+
+
+    // Control Signals
+    logic [1:0]             HTRANS ;   // Transfer type
+    logic                   HWRITE ;   // Write enable
+    logic [2:0]             HSIZE ;    // Transfer size
+    logic [2:0]             HBURST ;   // Burst type
+    logic [3:0]             HPROT ;    // Protection contro
+    // Address and Data
+    logic [ADDR_WIDTH-1:0]  HADDR;     // Address bus
+    logic [DATA_WIDTH-1:0]  HWDATA;    // Write data bus
+    logic [DATA_WIDTH-1:0]  HRDATA;    // Read data bus
+    // Transfer Response
+    logic                    HREADY;    // Transfer done
+    logic                    HRESP  ;  // Transfer response
+    //Additional Signals to LSU
+	logic     				o_mngr_sub_unsign;
+    // Master Interface Signals
+    logic                    req_ready ; // Ready to accept request
+    logic                    resp_valid; // Response valid,having data for CPU reading
+    logic [DATA_WIDTH-1:0]   resp_rdata ; // Read data (ld_data_o)
     // Declaration of Interim Wires
     wire [31:0] ld_data_M;
 
@@ -32,6 +56,70 @@ module memory_cycle(
     reg [31:0] ld_data_M_r;
 
     // Declaration of Module Initiation
+	// Connect to Manager_AHB
+	 // Clock and Reset
+Manager_AHB Master(
+    .HCLK(clk_i),
+    .HRESETn(rst_ni),
+    .HTRANS(HTRANS), 
+    .HWRITE(HWRITE),
+    .HSIZE(HSIZE),     // Transfer size***
+    .HBURST(BURST),    // Burst type***
+    .HPROT(HPROT),  
+    .HADDR(HADDR),     // Address bus
+    .HWDATA(HWDATA),
+    .HRDATA(HRDATA),
+    .HREADY(1'b1), 
+    .HRESP(1'b0),     // Transfer response***
+	.i_core_mngr_unsign(memory_signals.mem_unsign),
+	.o_mngr_sub_unsign(unsign),//****
+    .req_read(memory_signals.mem_load), 
+    .req_write(memory_signals.mem_wren),  
+    .req_addr(Result_M),
+    .req_wdata(rs2_data_M),  
+    .req_size(memory_signals.mem_size),   
+    .req_burst(3'd0),  // Burst type***		
+    .req_ready(req_ready),  // Ready to accept request***
+    .resp_valid(resp_valid), 
+    .resp_rdata(ld_data_M)  
+); 	
+
+
+	/*Subordinate_LSU Slave(
+	.HCLK(clk_i),
+	.HRESETn(rst_ni),
+	.HSEL_MEM(HSEL_MEM),//****
+	.HADDR(HADDR),
+	.HWRITE(HWRITE),
+	.HSIZE(HSIZE) ,
+	.HBURST(BURST),
+	.HPROT(HPROT),
+	.HTRANS(HTRANS),
+	.HMASTLOCK(1'b0),
+	.HREADY(HREADY)	,
+	.HWDATA(HWDATA),
+	.HRDATA(HRDATA)	,
+	.HREADYOUT(HREADYOUT),//***
+	.HRESP(HRESP), //***
+	.unsign_i(unsign),
+	.io_button(),
+    .io_sw(io_sw_i),
+	.ld_data_o(HRDATA_MEM),
+    .io_lcd(io_lcd_o),
+    .io_ledg(io_ledg_o),
+    .io_ledr(io_ledr_o),
+    .io_hex0(io_hex0_o),
+    .io_hex1(io_hex1_o),
+    .io_hex2(io_hex2_o),
+    .io_hex3(io_hex3_o),
+    .io_hex4(io_hex4_o),
+    .io_hex5(io_hex5_o),
+    .io_hex6(io_hex6_o),
+    .io_hex7(io_hex7_o)
+);
+	
+	
+/*
 	lsu lsu(
 		.clk_i(clk_i),
 		.rst_ni(rst_ni),
@@ -55,7 +143,7 @@ module memory_cycle(
 		.io_hex6(io_hex6_o),
 		.io_hex7(io_hex7_o) 
 	);
-
+*/
     // Memory Stage Register Logic
 	always @(posedge clk_i or negedge rst_ni) begin
 		if (~rst_ni) begin
