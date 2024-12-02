9b494544f1a7f707d902bb5dbdb2cc19bd4b9525
quyle663 <167063502+quyle663@users.noreply.github.com>
2024-12-02 21:48:31 +0700

    Update instr_memory.sv

--- 01_IF/instr_memory.sv
+++ 01_IF/instr_memory.sv
@@ -1,9 +1,9 @@
 module instr_memory #(
   parameter int unsigned IMEM_W = 13
 ) (
-  input  logic              clk_i   ,
-  input  logic              rst_ni	,
-  input  logic [IMEM_W-1:0] addr_i ,
+  input  logic              clk_i ,
+  input  logic              rst_ni,
+  input  logic [IMEM_W-1:0] addr_i,
   output logic [31:0]       instr_o
 );
   logic [3:0][7:0] imem [2**(IMEM_W-2)-1:0];
@@ -15,6 +15,7 @@ module instr_memory #(
   always_comb begin : proc_data
       instr_o = imem[addr_i[IMEM_W-1:2]][3:0];
   end
+  
 endmodule : instr_memory
 
 
